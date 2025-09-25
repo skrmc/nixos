112387bccfc178f3d1e62e8beb657729fe13ca23
SAKURA <Yejia995@gmail.com>
2025-09-25 17:22:31 -0400

    1758835351

--- src/share/system.nix
+++ src/share/system.nix
@@ -24,14 +24,16 @@
   ];
 
   # Network Settings
+  services.resolved.enable = true;
   services.tailscale.enable = true;
   networking = {
+    useDHCP = true;
+    useNetworkd = true;
     firewall.enable = false;
     wireless.iwd.enable = true;
     # networkmanager.enable = true;
     # networkmanager.wifi.backend = "iwd";
   };
-
   systemd.network = {
     enable = true;
     wait-online.enable = true;
