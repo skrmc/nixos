fbbde7a5a49c14adef4fa12f1eef21d25d2c2cd6
SAKURA <Yejia995@gmail.com>
2025-10-08 12:40:12 -0400

    1759941612

--- src/share/default.nix
+++ src/share/default.nix
@@ -1,7 +1,7 @@
 # /etc/nixos/src/share/default.nix
 {
   imports = [
-    # ./overlays.nix
+    ./overlays.nix
     ./system.nix
     ./users.nix
     ./home.nix
--- src/share/home/hyprland.nix
+++ src/share/home/hyprland.nix
@@ -6,18 +6,20 @@
   colors,
   ...
 }:
+let
+  MOD = "SUPER";
+  TERM = "foot";
+  MENU = "fuzzel";
+  BORDER = "1";
+  GAPS_IN = "2";
+  GAPS_OUT = "4";
+  ROUNDING = "8";
+in
 {
   wayland.windowManager.hyprland = {
     enable = true;
     plugins = with pkgs.hyprlandPlugins; [ hyprexpo ];
     settings = {
-      "$MOD" = "SUPER";
-      "$TERM" = "foot";
-      "$MENU" = "fuzzel";
-      "$BORDER" = "1";
-      "$GAPS_IN" = "2";
-      "$GAPS_OUT" = "4";
-      "$ROUNDING" = "8";
       xwayland = {
         enabled = true;
         force_zero_scaling = true;
@@ -28,7 +30,7 @@
         "wl-paste --type image --watch cliphist store"
 
         "quickshell"
-        "[workspace 1 silent] $MENU"
+        "[workspace 1 silent] ${MENU}"
         "[workspace 10 silent; tile] foot -e btop"
         # "find $HOME/.local/share/wallpapers -maxdepth 1 -type f -print | head -n1 | xargs -r swaybg -m fill -i"
       ];
@@ -59,84 +61,84 @@
           size = 6;
         };
         shadow.enabled = false;
-        rounding = "$ROUNDING";
+        rounding = "${ROUNDING}";
         inactive_opacity = "0.95";
       };
       general = {
         "allow_tearing" = true;
-        "gaps_in" = "$GAPS_IN";
-        "gaps_out" = "$GAPS_OUT";
-        "border_size" = "$BORDER";
+        "gaps_in" = "${GAPS_IN}";
+        "gaps_out" = "${GAPS_OUT}";
+        "border_size" = "${BORDER}";
         "col.active_border" = "rgb(${colors."70"})";
         "col.inactive_border" = "rgb(${colors."30"})";
       };
       gesture = "3, horizontal, scale: 1.5, workspace";
       bind = [
-        "$MOD, RETURN, exec, $TERM"
-        "$MOD, E, exec, $TERM -e yazi"
-        "$MOD, Q, killactive"
-        "$MOD, C, centerwindow"
-        "$MOD, F, fullscreen, 0"
-        "$MOD, T, togglefloating"
-        "$MOD, D, exec, pkill $MENU || $MENU"
-        # "$MOD, P, pseudo"
-        # "$MOD, J, togglesplit"
-        "$MOD, UP, movefocus, u"
-        "$MOD, DOWN, movefocus, d"
-        "$MOD, LEFT, movefocus, l"
-        "$MOD, RIGHT, movefocus, r"
-        "$MOD SHIFT, LEFT, movewindow, l"
-        "$MOD SHIFT, RIGHT, movewindow, r"
-        "$MOD SHIFT, UP, movewindow, u"
-        "$MOD SHIFT, DOWN, movewindow, d"
-        "$MOD, O, hyprexpo:expo, toggle"
-        "$MOD, 1, workspace, 1"
-        "$MOD, 2, workspace, 2"
-        "$MOD, 3, workspace, 3"
-        "$MOD, 4, workspace, 4"
-        "$MOD, 5, workspace, 5"
-        "$MOD, 6, workspace, 6"
-        "$MOD, 7, workspace, 7"
-        "$MOD, 8, workspace, 8"
-        "$MOD, 9, workspace, 9"
-        "$MOD, 0, workspace, 10"
-        "$MOD SHIFT, 1, movetoworkspace, 1"
-        "$MOD SHIFT, 2, movetoworkspace, 2"
-        "$MOD SHIFT, 3, movetoworkspace, 3"
-        "$MOD SHIFT, 4, movetoworkspace, 4"
-        "$MOD SHIFT, 5, movetoworkspace, 5"
-        "$MOD SHIFT, 6, movetoworkspace, 6"
-        "$MOD SHIFT, 7, movetoworkspace, 7"
-        "$MOD SHIFT, 8, movetoworkspace, 8"
-        "$MOD SHIFT, 9, movetoworkspace, 9"
-        "$MOD SHIFT, 0, movetoworkspace, 10"
-        "$MOD CTRL, LEFT, workspace, -1"
-        "$MOD CTRL, RIGHT, workspace, +1"
-        "$MOD SHIFT, LEFT, movetoworkspace, -1"
-        "$MOD SHIFT, RIGHT, movetoworkspace, +1"
-        "$MOD, TAB, togglespecialworkspace, scratch"
-        "$MOD SHIFT, TAB, movetoworkspace, special:scratch"
-        "$MOD, ESCAPE, movetoworkspacesilent, special:scratch"
-        "$MOD SHIFT, S, exec, grim -g \"$(slurp -d)\" - | wl-copy"
-        "$MOD SHIFT, C, exec, hyprpicker -a"
-        "$MOD, V, exec, cliphist list | $MENU --dmenu | cliphist decode | wl-copy"
+        "${MOD}, RETURN, exec, ${TERM}"
+        "${MOD}, E, exec, ${TERM} -e yazi"
+        "${MOD}, Q, killactive"
+        "${MOD}, C, centerwindow"
+        "${MOD}, F, fullscreen, 0"
+        "${MOD}, T, togglefloating"
+        "${MOD}, D, exec, pkill ${MENU} || ${MENU}"
+        # "${MOD}, P, pseudo"
+        # "${MOD}, J, togglesplit"
+        "${MOD}, UP, movefocus, u"
+        "${MOD}, DOWN, movefocus, d"
+        "${MOD}, LEFT, movefocus, l"
+        "${MOD}, RIGHT, movefocus, r"
+        "${MOD} SHIFT, LEFT, movewindow, l"
+        "${MOD} SHIFT, RIGHT, movewindow, r"
+        "${MOD} SHIFT, UP, movewindow, u"
+        "${MOD} SHIFT, DOWN, movewindow, d"
+        "${MOD}, O, hyprexpo:expo, toggle"
+        "${MOD}, 1, workspace, 1"
+        "${MOD}, 2, workspace, 2"
+        "${MOD}, 3, workspace, 3"
+        "${MOD}, 4, workspace, 4"
+        "${MOD}, 5, workspace, 5"
+        "${MOD}, 6, workspace, 6"
+        "${MOD}, 7, workspace, 7"
+        "${MOD}, 8, workspace, 8"
+        "${MOD}, 9, workspace, 9"
+        "${MOD}, 0, workspace, 10"
+        "${MOD} SHIFT, 1, movetoworkspace, 1"
+        "${MOD} SHIFT, 2, movetoworkspace, 2"
+        "${MOD} SHIFT, 3, movetoworkspace, 3"
+        "${MOD} SHIFT, 4, movetoworkspace, 4"
+        "${MOD} SHIFT, 5, movetoworkspace, 5"
+        "${MOD} SHIFT, 6, movetoworkspace, 6"
+        "${MOD} SHIFT, 7, movetoworkspace, 7"
+        "${MOD} SHIFT, 8, movetoworkspace, 8"
+        "${MOD} SHIFT, 9, movetoworkspace, 9"
+        "${MOD} SHIFT, 0, movetoworkspace, 10"
+        "${MOD} CTRL, LEFT, workspace, -1"
+        "${MOD} CTRL, RIGHT, workspace, +1"
+        "${MOD} SHIFT, LEFT, movetoworkspace, -1"
+        "${MOD} SHIFT, RIGHT, movetoworkspace, +1"
+        "${MOD}, TAB, togglespecialworkspace, scratch"
+        "${MOD} SHIFT, TAB, movetoworkspace, special:scratch"
+        "${MOD}, ESCAPE, movetoworkspacesilent, special:scratch"
+        "${MOD} SHIFT, S, exec, grim -g \"$(slurp -d)\" - | wl-copy"
+        "${MOD} SHIFT, C, exec, hyprpicker -a"
+        "${MOD}, V, exec, cliphist list | ${MENU} --dmenu | cliphist decode | wl-copy"
       ];
       # bindr = [
-      #   "$MOD, $MOD_L, exec, pkill $MENU || $MENU"
+      #   "${MOD}, ${MOD}_L, exec, pkill ${MENU} || ${MENU}"
       # ];
       bindm = [
-        "$MOD, mouse:272, movewindow"
-        "$MOD, mouse:273, resizewindow"
-        "$MOD, Z, movewindow"
+        "${MOD}, mouse:272, movewindow"
+        "${MOD}, mouse:273, resizewindow"
+        "${MOD}, Z, movewindow"
       ];
       bindl = [
         ", XF86AudioMute, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 0%"
-        "$MOD+SHIFT, M, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 0%"
+        "${MOD}+SHIFT, M, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 0%"
         ", Print, exec, grim - | wl-copy"
-        "$MOD+SHIFT, B, exec, playerctl previous"
-        "$MOD+SHIFT, P, exec, playerctl play-pause"
+        "${MOD}+SHIFT, B, exec, playerctl previous"
+        "${MOD}+SHIFT, P, exec, playerctl play-pause"
         ", XF86AudioPlay, exec, playerctl play-pause"
-        "$MOD+SHIFT, L, exec, systemctl suspend"
+        "${MOD}+SHIFT, L, exec, systemctl suspend"
       ];
       bindle = [
         ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
@@ -145,8 +147,8 @@
         ", XF86MonBrightnessDown, exec, brightnessctl s 5%-"
       ];
       binde = [
-        "$MOD, MINUS, splitratio, -0.1"
-        "$MOD, EQUAL, splitratio, 0.1"
+        "${MOD}, MINUS, splitratio, -0.1"
+        "${MOD}, EQUAL, splitratio, 0.1"
       ];
       workspace = [
         "w[tv1]s[0], gapsout:0, gapsin:0"
@@ -155,19 +157,17 @@
       windowrule = [
         "immediate, fullscreen:0"
         "float, title:(.*)(File)(.*)"
-        "float, opacity 0.925, class:$TERM"
-        "bordersize 0, floating:0, onworkspace:w[tv1]s[0]"
-        "rounding 0, floating:0, onworkspace:w[tv1]s[0]"
-        "bordersize 0, floating:0, onworkspace:f[1]s[0]"
-        "rounding 0, floating:0, onworkspace:f[1]s[0]"
+        "float, opacity 0.9, class:${TERM}"
         "opacity 0.75, floating:1, focus:0"
+        "bordersize 0, rounding 0, floating:0, onworkspace:w[tv1]s[0]"
+        "bordersize 0, rounding 0, floating:0, onworkspace:f[1]s[0]"
       ];
       layerrule = [
         "noanim, selection"
       ];
       misc = {
         enable_swallow = true;
-        swallow_regex = "$TERM";
+        swallow_regex = "${TERM}";
         font_family = fonts.sans;
         disable_hyprland_logo = true;
         background_color = "0x${colors."0"}";
