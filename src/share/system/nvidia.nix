5bd08713d71ba68e0f5f7ccfe26d16f6c6a40c1a
SAKURA <Yejia995@gmail.com>
2025-10-26 18:58:42 -0400

    1761519522

--- src/share/system/nvidia.nix
+++ src/share/system/nvidia.nix
@@ -25,7 +25,7 @@
       # forceFullCompositionPipeline = true;
       open = true;
       nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.beta;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
   };
 }
