a115a64ac4c7495b9158758381cedaa2e4fa4bce
SAKURA <Yejia995@gmail.com>
2025-12-20 17:08:54 -0500

    1766268534

--- src/hosts/SAKURA/default.nix
+++ src/hosts/SAKURA/default.nix
@@ -27,6 +27,14 @@
     }
   );
 
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.kernelModules = [ "v4l2loopback" ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="Integrated Camera" exclusive_caps=1
+  '';
+
   # Home Manager
   home-manager.users.${user} = {
     wayland.windowManager.hyprland = {
