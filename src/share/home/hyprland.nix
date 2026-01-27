c7dc53cfa835c9c6f50ee6df41644ba70ad7fcd9
SAKURA <Yejia995@gmail.com>
2026-01-27 13:50:40 -0500

    1769539840

--- src/share/home/hyprland.nix
+++ src/share/home/hyprland.nix
@@ -154,8 +154,8 @@ in
         "${MOD}+SHIFT, L, exec, systemctl suspend"
       ];
       bindle = [
-        ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
-        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 2%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%-"
         ", XF86MonBrightnessUp, exec, brightnessctl s +5%"
         ", XF86MonBrightnessDown, exec, brightnessctl s 5%-"
       ];
